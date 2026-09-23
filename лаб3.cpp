25312e38fe4c528d568633c2240b762e94b984b9
unknown <a.cherkaj@mail.ru>
2026-09-23 19:55:06 +0500

    Добавлена функция создания SafeArray

--- лаб3.cpp
+++ лаб3.cpp
@@ -5,7 +5,14 @@ struct SafeArray
     int* data;
     int size;
 };
+SafeArray createArray(int size)
+{
+    SafeArray arr;
+    arr.data = new int[size]{};
+    arr.size = size;
 
+    return arr;
+}
 int main()
 {
     return 0;
