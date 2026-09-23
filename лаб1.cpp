6bb5993a3fbaeac7e4fcc466cae24e1a1f9a667a
unknown <a.cherkaj@mail.ru>
2026-09-23 18:43:25 +0500

    Добавлена функция обмена элементов массива

--- лаб1.cpp
+++ лаб1.cpp
@@ -17,13 +17,19 @@ void printArray(const int (&arr)[10])
 
     std::cout << std::endl;
 }
-
+void swapElements(int (&arr)[10], int index1, int index2)
+{
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
 int main()
 {
     int arr[10]{};
 
     fillArray(arr);
     printArray(arr);
-
+    swapElements(arr, 0, 9);
+    printArray(arr);
     return 0;
 }
