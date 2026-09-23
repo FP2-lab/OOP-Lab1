df4b34170f683b9070f4f429828e0ddb5f705238
unknown <a.cherkaj@mail.ru>
2026-09-23 21:13:29 +0500

    Добавлена функция вывода матрицы

--- лаб4.cpp
+++ лаб4.cpp
@@ -23,7 +23,53 @@ void fillMatrix(int** matrix, int rows, int cols)
         }
     }
 }
+void printMatrix(int** matrix, int rows, int cols, bool showBorders = true, std::string title = "Matrix")
+{
+    std::cout << title << std::endl;
+
+    if (showBorders)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            std::cout << "----";
+        }
+
+        std::cout << "-" << std::endl;
+    }
+
+    for (int i = 0; i < rows; ++i)
+    {
+        if (showBorders)
+        {
+            std::cout << "| ";
+        }
+
+        for (int j = 0; j < cols; ++j)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+
+        if (showBorders)
+        {
+            std::cout << "|" << std::endl;
+        }
+        else
+        {
+            std::cout << std::endl;
+        }
+    }
+
+    if (showBorders)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            std::cout << "----";
+        }
+
+        std::cout << "-" << std::endl;
+    }
+}
 int main()
 {
-    return 0;
+    return 0;   
 }
