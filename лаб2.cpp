d0122965a82bb9a525ff1164b6c746cd3b4ad6d3
unknown <a.cherkaj@mail.ru>
2026-09-23 19:24:52 +0500

    Добавлена обработка массива до первого отрицательного элемента

--- лаб2.cpp
+++ лаб2.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+void process(int*& arr, int size)
+{
+    int index = -1;
+
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] < 0)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1)
+    {
+        int* newArr = new int[index];
+
+        for (int i = 0; i < index; ++i)
+        {
+            newArr[i] = arr[i];
+        }
+
+        delete[] arr;
+        arr = newArr;
+    }
+}
+
 int main()
 {
     int N;
@@ -11,6 +38,26 @@ int main()
     {
         std::cin >> arr[i];
     }
+
+    int newSize = N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        if (arr[i] < 0)
+        {
+            newSize = i;
+            break;
+        }
+    }
+
+    process(arr, N);
+
+    for (int i = 0; i < newSize; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
