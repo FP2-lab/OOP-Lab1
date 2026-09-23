de3ff0b8345d20dce01d42f9fa14bf2eed01665e
unknown <a.cherkaj@mail.ru>
2026-09-23 18:35:05 +0500

    Добавлена функция вывода массива

--- лаб1.cpp
+++ лаб1.cpp
@@ -8,11 +8,22 @@ void fillArray(int (&arr)[10])
     }
 }
 
+void printArray(const int (&arr)[10])
+{
+    for (auto x : arr)
+    {
+        std::cout << x << " ";
+    }
+
+    std::cout << std::endl;
+}
+
 int main()
 {
     int arr[10]{};
 
     fillArray(arr);
+    printArray(arr);
 
     return 0;
 }
